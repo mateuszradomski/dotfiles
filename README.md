40756c228d5ccadf0c6c09d67e7fb14f791b2049
Mateusz Radomski <radomski.main@protonmail.com>
2022-06-15 20:54:34 +0200

    Everything up to this point

---
+++ README.md
@@ -0,0 +1,5 @@
+To install run:
+
+``` bash
+curl --proto '=https' --tlsv1.2 https://raw.githubusercontent.com/m-radomski/dotfiles/master/setup.sh | bash
+```
--- setup.sh
+++ setup.sh
@@ -1,2 +1,47 @@
 #!/bin/sh
 
+NEOVIM_BINARY_URL="https://github.com/neovim/neovim/releases/download/v0.7.0/nvim.appimage"
+NEOVIM_BINARY_PATH="$HOME/.local/bin/nvim"
+
+BASE_URL="https://raw.githubusercontent.com/m-radomski/dotfiles/master"
+TMUX_CONF_PATH="$HOME/.tmux.conf"
+NVIM_INIT_PATH="$HOME/.config/nvim/init.vim"
+
+YELLOW='\033[93m'
+GREEN='\033[92m'
+RESET='\033[0m'
+
+# Download my dotfiles
+curl --proto '=https' --tlsv1.2 --silent --location $BASE_URL/.tmux.conf --create-dirs --output $TMUX_CONF_PATH
+curl --proto '=https' --tlsv1.2 --silent --location $BASE_URL/init.vim --create-dirs --output $NVIM_INIT_PATH
+
+echo -e $YELLOW"Downloaded the config files"$RESET
+
+# Remove the old nvim
+rm -rf $(which nvim)
+
+# Download the chosen neovim version and install it locally
+curl --proto '=https' --tlsv1.2 --silent --location $NEOVIM_BINARY_URL --create-dirs --output $NEOVIM_BINARY_PATH
+chmod +x $NEOVIM_BINARY_PATH
+
+echo -e $YELLOW"Downloaded the neovim binary"$RESET
+ 
+# Download the vim-plug [https://github.com/junegunn/vim-plug]
+curl --proto '=https' --tlsv1.2 --silent -fLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo -e $YELLOW"Downloaded the vim-plug"$RESET
+
+# To ignore errors export only pluged files
+PLUG_TMP_FILE=$(mktemp)
+awk '/plug#begin/,/plug#end/' $NVIM_INIT_PATH > $PLUG_TMP_FILE
+$NEOVIM_BINARY_PATH -u $PLUG_TMP_FILE --headless +PlugInstall +qall &> /dev/null
+rm $PLUG_TMP_FILE
+
+echo -e $YELLOW"Downloaded all plugins"$RESET
+
+# Change the default editor to nvim  
+# remove the old alias if already created
+sed '/alias vim/d' -i $HOME/.bashrc 
+echo 'alias vim=nvim' >> $HOME/.bashrc
+
+echo -e $GREEN"Finished, happy coding..."$RESET
