711a43ce1310bb480c928926dc071c47be89adcb
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-05-09 13:44:22 +0200

    Appimage is not that stable

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-NEOVIM_BINARY_URL="https://github.com/neovim/neovim/releases/download/v0.7.0/nvim.appimage"
-NEOVIM_BINARY_PATH="$HOME/.local/bin/nvim"
-
 BASE_URL="https://raw.githubusercontent.com/mateuszradomski/dotfiles/master"
 TMUX_CONF_PATH="$HOME/.tmux.conf"
 NVIM_INIT_PATH="$HOME/.config/nvim/init.vim"
@@ -25,15 +22,6 @@ fi
 
 echo -e $YELLOW"Downloaded the config files"$RESET
 
-# Remove the old nvim
-rm -rf $(which nvim)
-
-# Download the chosen neovim version and install it locally
-curl --proto '=https' --tlsv1.2 --silent --location $NEOVIM_BINARY_URL --create-dirs --output $NEOVIM_BINARY_PATH
-chmod +x $NEOVIM_BINARY_PATH
-
-echo -e $YELLOW"Downloaded the neovim binary"$RESET
- 
 # Download the vim-plug [https://github.com/junegunn/vim-plug]
 curl --proto '=https' --tlsv1.2 --silent -fLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
@@ -42,7 +30,7 @@ echo -e $YELLOW"Downloaded the vim-plug"$RESET
 # To ignore errors export only pluged files
 PLUG_TMP_FILE=$(mktemp)
 awk '/plug#begin/,/plug#end/' $NVIM_INIT_PATH > $PLUG_TMP_FILE
-$NEOVIM_BINARY_PATH -u $PLUG_TMP_FILE --headless +PlugInstall +qall &> /dev/null
+nvim -u $PLUG_TMP_FILE --headless +PlugInstall +qall &> /dev/null
 rm $PLUG_TMP_FILE
 
 echo -e $YELLOW"Downloaded all plugins"$RESET
